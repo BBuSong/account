3ba2d008e3e6e835017ec59eeba7033ae152fdb2
minhyo <minhyo17@naver.com>
2024-05-26 22:53:54 +0900

    Account 잔액 사용 취소

--- src/main/java/org/example/account/controller/TransactionController.java
+++ src/main/java/org/example/account/controller/TransactionController.java
@@ -3,6 +3,7 @@ package org.example.account.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.account.dto.CancelBalance;
 import org.example.account.dto.TransactionDto;
 import org.example.account.dto.UseBalance;
 import org.example.account.exception.AccountException;
@@ -23,8 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionController {
     private final TransactionService transactionService;
 
-    @PostMapping("/transaction/user")
-    public UseBalance.Response userBalance(
+    @PostMapping("/transaction/use")
+    public UseBalance.Response useBalance(
             @Valid @RequestBody UseBalance.Request request
     ){
         try {
@@ -36,6 +37,27 @@ public class TransactionController {
             log.error("Failed to use balance. ");
 
             transactionService.saveFailedUseTransaction(
+                    request.getAccountNumber(),
+                    request.getAmount()
+            );
+
+            throw e;
+        }
+    }
+
+    @PostMapping("/transaction/cancel")
+    public CancelBalance.Response cancelBalance(
+            @Valid @RequestBody CancelBalance.Request request
+    ){
+        try {
+            return CancelBalance.Response.from(
+                    transactionService.cancelBalance(request.getTransactionId(),
+                            request.getAccountNumber(), request.getAmount())
+            );
+        }catch (AccountException e) {
+            log.error("Failed to use balance. ");
+
+            transactionService.saveFailedCancelTransaction(
                     request.getAccountNumber(),
                     request.getAmount()
             );
--- src/main/java/org/example/account/repository/TransactionRepository.java
+++ src/main/java/org/example/account/repository/TransactionRepository.java
@@ -6,8 +6,11 @@ import org.example.account.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TransactionRepository
         extends JpaRepository<Transaction, Long> {
 
+    Optional<Transaction> findByTransactionId(String transactionId);
 }
